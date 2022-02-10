6d78ca181e5915e52b48399c82aa37e1609584c5
Dohyun Park <dhlife09@gmail.com>
2022-02-11 02:28:25 +0900

    Create List.groovy

---
+++ List.groovy
@@ -0,0 +1,7 @@
+//리스트
+customList = [111, 222, 333, 444, 555]
+println customList[2]    //배열처럼 사용가능
+println customList.get(444) //원래대로 customList의 get() 호출 가능
+println customList.size()   //result=5
+
+emptyList = []  //빈 리스트 생성
